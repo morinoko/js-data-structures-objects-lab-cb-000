702ab4f724b4d85e82089edc2354fb1de4b54f4d
Felice Forby <feliceforby@gmail.com>
2018-12-28 00:37:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 // Write your solution in this file!
 const driver = {};
+
+function updateDriverWithKeyAndValue() {
+  
+}
+
+function destructivelyUpdateDriverWithKeyAndValue() {
+  
+}
+
+function deleteFromDriverByKey() {
+  
+}
+
+function destructivelyDeleteFromDriverByKey() {
+  
+}
