564ffb853a49cd5100b2f5ad1bfdb6f245ceec2c
Felice Forby <feliceforby@gmail.com>
2018-12-28 00:40:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value) {
   // let newDriver = {};
-  // 
+  //
   // return Oject.assign(newDriver, )
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value;
+  return driver;
 }
 
 function deleteFromDriverByKey() {
